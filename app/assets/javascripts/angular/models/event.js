624005dc1541302e9433994e073ec83250111429
MatthieuLPro <matthieu.lu.pro@gmail.com>
2019-06-20 09:34:27 +0200

    Revert "to delete"
    This reverts commit 7def42128fb5f62a757bdd349d87406d4d56ff38.

--- app/assets/javascripts/angular/models/event.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('angularExample');
-
-app.factory('Event', ['$resource', function($resource) {
-  return $resource('/api/events/:id.json', { id: '@id' });
-}]);
--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  root 'dashboard#index'
-
-  namespace :api do
-	  resources :events, only: [:index, :create]
-  end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
